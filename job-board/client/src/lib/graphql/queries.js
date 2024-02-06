d1ca3414a944e499d8cee9f1afd7354b0e6c1c61
Justin Large <justin.large@keap.com>
2024-02-06 07:16:17 -0700

    Implemented create job on the client side

--- job-board/client/src/lib/graphql/queries.js
+++ job-board/client/src/lib/graphql/queries.js
@@ -42,6 +42,18 @@ export async function getJobs() {
     return jobs;
 }
 
+export async function createJob({ title, description }) {
+  const mutation  = gql`
+    mutation CreateJob($input: CreateJobInput!) {
+      job: createJob(input: $input) {
+        id
+      }
+    }
+  `
+  const { job } = await client.request(mutation, { input: { title, description } });
+  return job;
+}
+
 export async function getCompany(id) {
   const query  = gql`
     query ($id: ID!) {
